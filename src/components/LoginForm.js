9a2a48f97def553886e6a215496c76442dadcb17
entcheva <christina.entcheva@gmail.com>
2017-10-26 17:30:17 -0400

    Build out signin/signup logic

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
+  };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(() => {
+          this.setState({ error: 'Authentication Failed.'});
+        });
+      });
   };
 
   render() {
@@ -29,10 +44,24 @@ export default class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>Log in</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Log in
+          </Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
