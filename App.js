554b304ed71e1cab23243a11b862cd36b8fe88b3
entcheva <christina.entcheva@gmail.com>
2017-10-26 14:06:26 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
+
+export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC915WSqDQDQduDIxZKUJVUAtOdTplhMQg',
+      authDomain: 'auth-acb21.firebaseapp.com',
+      databaseURL: 'https://auth-acb21.firebaseio.com',
+      projectId: 'auth-acb21',
+      storageBucket: '',
+      messagingSenderId: '13828717977'
+    })
+  }
+
+  render() {
+    return (
+    <View>
+      <Header headerText='Authentication' />
+      <LoginForm />
+    </View>
+    );
+  }
+};
