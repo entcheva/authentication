5b589639b0035e50c085f8abc34ac43bfd371a2b
entcheva <christina.entcheva@gmail.com>
2017-10-30 16:14:37 -0400

    Log out a user when Log Out button is tapped

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { Header, Button, Spinner } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 export default class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyC915WSqDQDQduDIxZKUJVUAtOdTplhMQg',
@@ -14,14 +16,38 @@ export default class App extends Component {
       storageBucket: 'auth-acb21.appspot.com',
       messagingSenderId: '13828717977'
     });
-  };
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />
+    }
+  }
 
   render() {
     return (
     <View>
       <Header headerText='Authentication' />
-      <LoginForm />
+      {this.renderContent()}
     </View>
     );
-  };
-};
+  }
+
+}
